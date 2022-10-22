2bd8443a7d1d5f2e440656a9d5609b8501874c99
Alex Antonakakis <antonakakis1@gmail.com>
2022-10-22 03:50:38 +0300

    Improves error messages

--- src/turnin.c
+++ src/turnin.c
@@ -223,7 +223,7 @@ void be_class() {
 	}
 
 	if (seteuid(class_uid) == -1) {
-		perror("seteuid");
+		perror("seteuid class");
 		exit(1);
 	}
 }
@@ -243,7 +243,7 @@ void be_user() {
 		exit(1);
 	}
 	if (seteuid(user_uid) == -1) {
-		perror("seteuid");
+		perror("seteuid user");
 		exit(1);
 	}
 }
